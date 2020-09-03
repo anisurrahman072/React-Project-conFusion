9e1f2cc140ac223da4f5c1154fbdd49e7e50b943
Md Anisur Rahman <anisurrahmanbup@gmail.com>
2020-09-03 19:08:34 +0600

    Week2_Assignment2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,7 @@ import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -48,6 +49,7 @@ class Main extends Component {
           <Route exact path='/Menu' component={() => <Menu dishes = {this.state.dishes} />} />
           <Route path='/menu/:dishId' component={ DishWithId } />
           <Route exact path='/contactus' component={ Contact } />
+          <Route exact path='/aboutus' component={() => <About leaders = {this.state.leaders} />} />
           <Redirect to='/Home' />
         </Switch>
         <Footer />
